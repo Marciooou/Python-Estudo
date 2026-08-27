0cf45fb13a3d24d5c00c3c5e2b1b46082f2804aa
Marcio Ramos <marcio-augusto22@hotmail.com>
2026-08-26 21:22:06 -0300

    Subindo aula

--- Estudo/entrada&saida.py
+++ Estudo/entrada&saida.py
@@ -12,5 +12,3 @@ nome = "Juan"
 idade = 25
 
 print(f"Olá, meu nome é {nome} e tenho {idade} anos!")
-
-
---
+++ Estudo_2/Aula_2.py
@@ -0,0 +1,19 @@
+
+#\n quebra a linha
+nome = " Fala fiote "
+nome_dois = " fala fioootee "
+print(51 , 52, sep= nome, end = nome_dois)
+
+print ('deixando mais "simples"')
+
+#float
+print(1.5)
+
+print("========")
+
+#type: mostra a função
+print(type("oa"))
+
+#booleano
+print(5 == 10)
+print(25>20)
